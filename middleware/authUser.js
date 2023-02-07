1a0b943e4386afebb7b5362842a9632c52c64cdd
Manan Doshi <MANANDOSHI1301@GMAIL.COM>
2023-02-07 11:36:12 +0530

    Configured with bearer

--- middleware/authUser.js
+++ middleware/authUser.js
@@ -2,7 +2,8 @@ const jwt = require('jsonwebtoken');
 
 const authUser = (req, res, next) => {
 
-  const token = req.header('authToken');
+  const tokenString = req.header('authorization');
+  const token = tokenString && tokenString.split(" ")[1];
   if (!token) {
     res.status(401).json({
       message: "Please authenticate using a valid token",
@@ -10,8 +11,15 @@ const authUser = (req, res, next) => {
     })
   }
   try {
-    const data = jwt.verify(token, process.env.JWT_ACCESS_TOKEN);
-    req.data = data;
+    const data = jwt.verify(token, process.env.JWT_ACCESS_TOKEN, (err, user) => {
+      if (err) {
+        res.status(403).json({
+          message: "Please authenticate using a valid token",
+          error: true
+        })
+      }
+      req.user = user;
+    });
     next();
   } catch (error) {
     console.log(error)
